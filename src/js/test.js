4c16467b10139c3775df66345f3239b0ec0c1c5a
Valentin Vladimirov <valentinvladimirov@abv.bg>
2022-12-02 11:21:40 +0200

    bug fix for task-52

--- src/js/test.js
+++ src/js/test.js
@@ -1,16 +1,20 @@
+var count = 0;
+
 function createArticles() {
     const length = 5;
 
     for (let i = 0; i < length; i++) {
         var x = document.createElement("article");
 
-        x.setAttribute("id", "myArticle" + i);
+        count += length;
+
+        x.setAttribute("id", "myArticle" + count);
         document.body.appendChild(x);
 
         var txt1 = document.createTextNode("some text...");
 
-        document.getElementById("myArticle" + i).appendChild(txt1);
-        document.getElementById("myArticle" + i).classList.add("message");
+        document.getElementById("myArticle" + count).appendChild(txt1);
+        document.getElementById("myArticle" + count).classList.add("message");
     }
 }
 
